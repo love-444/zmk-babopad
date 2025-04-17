327882dee08f96c83291919f487b4b1c778dfb54
사랑 나 <litmus337@gmail.com>
2025-04-17 20:32:25 +0900

    dfdf

--- src/babopad.c
+++ src/babopad.c
@@ -180,7 +180,7 @@ static void babopad_async_init(struct k_work *work) {
 
     for (size_t i = 0; i < config->adc_channels_size; i++)
     {
-        struct nrf_saadc_channel_config_t cfg = {
+        nrf_saadc_channel_config_t cfg = {
             .resistor_p = NRF_SAADC_RESISTOR_DISABLED,
             .resistor_n = NRF_SAADC_RESISTOR_ENABLED,
             .gain = NRF_SAADC_GAIN4,
