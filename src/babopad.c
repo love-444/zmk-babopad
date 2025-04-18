2b494f585b4fce0a24bc27e8e3d8977abee7f39f
사랑 나 <litmus337@gmail.com>
2025-04-18 22:11:50 +0900

    as

--- src/babopad.c
+++ src/babopad.c
@@ -219,7 +219,7 @@ static void babopad_async_init(struct k_work *work) {
                         10, NULL);
 
     k_timer_init(&data->sampling_timer, sampling_timer_handler, NULL);
-    k_timer_start(&data->sampling_timer, K_NO_WAIT, K_MSEC(1));
+    k_timer_start(&data->sampling_timer, K_NO_WAIT, K_MSEC(10));
 }
 
 static int babopad_init(const struct device *dev) {
