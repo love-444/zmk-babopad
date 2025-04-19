0fe484680293530de8bd3e98a13cd730a50d6a9d
Nan_o <litmus337@gmail.com>
2025-04-19 10:49:31 +0900

    ooooooo

--- src/babopad.c
+++ src/babopad.c
@@ -130,7 +130,7 @@ static void babopad_async_init(struct k_work *work) {
     gpio_pin_set_dt(&led, 0);
 
     k_work_init(&data->sampling_work, sampling_work_handler);
-    k_work_queue_start(&babopad_work_q, babopad_q_stack, K_THREAD_STACK_SIZEOF(babopad_q_stack), 1, NULL);
+    k_work_queue_start(&babopad_work_q, babopad_q_stack, K_THREAD_STACK_SIZEOF(babopad_q_stack), 50, NULL);
     k_timer_init(&data->sampling_timer, sampling_timer_handler, NULL);
     k_timer_start(&data->sampling_timer, K_MSEC(1), K_MSEC(1));
 }
