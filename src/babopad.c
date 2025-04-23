67ddf596c62d146a03767d2af5d27f39e9fe4f9d
Nan_o <litmus337@gmail.com>
2025-04-24 00:11:13 +0900

    adf

--- src/babopad.c
+++ src/babopad.c
@@ -130,7 +130,7 @@ static void babopad_async_init(struct k_work *work) {
     k_work_init(&data->sampling_work, sampling_work_handler);
     k_work_queue_start(&babopad_work_q, babopad_q_stack, K_THREAD_STACK_SIZEOF(babopad_q_stack), 50, NULL);
     k_timer_init(&data->sampling_timer, sampling_timer_handler, NULL);
-    k_timer_start(&data->sampling_timer, K_MSEC(1), K_MSEC(1));
+    k_timer_start(&data->sampling_timer, K_MSEC(10), K_MSEC(10));
 }
 
 static int babopad_init(const struct device *dev) {
