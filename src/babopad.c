d6fdc10b7d73093103cc013d52bb6af7b3e11de6
Nan_o <litmus337@gmail.com>
2025-04-24 02:40:32 +0900

    r

--- src/babopad.c
+++ src/babopad.c
@@ -98,7 +98,7 @@ static void babopad_async_init(struct k_work *work) {
             .reference = ADC_REF_INTERNAL,
             .acquisition_time = ADC_ACQ_TIME_DEFAULT,
             .channel_id = config->adc_channels[i],
-            .input_positive = SAADC_CH_PSELP_PSELP_AnalogInput0 + config->adc_channels[i],
+            .input_positive = config->adc_channels[i],
         };
         nrf_saadc_channel_config_t cfg = {
             .resistor_p = NRF_SAADC_RESISTOR_DISABLED,
