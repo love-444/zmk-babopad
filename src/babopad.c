62ef99d1c3161f9ad537af7ea8c803f2100671dd
Nan_o <litmus337@gmail.com>
2025-04-25 02:33:03 +0900

    ag

--- src/babopad.c
+++ src/babopad.c
@@ -133,7 +133,9 @@ static void babopad_async_init(struct k_work *work) {
         nrf_saadc_channel_input_set(NRF_SAADC, config->adc_channels[i], NRF_SAADC_INPUT_DISABLED, NRF_SAADC_INPUT_DISABLED);
     }
     // init pwm
-    pwm_set_dt(&pwm0, PWM_MSEC(500), PWM_MSEC(250));
+    pwm_set_dt(&pwm0, PWM_MSEC(50), PWM_MSEC(25));
+    pwm_set_dt(&pwm1, PWM_MSEC(50), PWM_MSEC(25));
+    pwm_set_dt(&pwm2, PWM_MSEC(50), PWM_MSEC(25));
 
     data->ready = true;
 
