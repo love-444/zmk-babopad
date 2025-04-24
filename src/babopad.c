cc846d4854de6bf04a60ebc7052f901f758db142
Nan_o <litmus337@gmail.com>
2025-04-24 23:54:34 +0900

    asd

--- src/babopad.c
+++ src/babopad.c
@@ -110,17 +110,17 @@ static void babopad_async_init(struct k_work *work) {
             .input_positive = config->adc_channels[i] + 1,
         };
         nrf_saadc_channel_config_t cfg = {
-            .resistor_p = NRF_SAADC_RESISTOR_DISABLED,
+            .resistor_p = NRF_SAADC_RESISTOR_PULL_DOWN,
             .resistor_n = NRF_SAADC_RESISTOR_DISABLED,
             .gain = NRF_SAADC_GAIN1_6,
             .reference = NRF_SAADC_REFERENCE_INTERNAL,
             .acq_time = NRF_SAADC_ACQTIME_10US,
-            .mode = NRF_SAADC_MODE_DIFFERENTIAL,
+            .mode = NRF_SAADC_MODE_SINGLE_ENDED,
             .burst = NRF_SAADC_BURST_DISABLED,
         };
         adc_channel_setup(adc, &_pl);
-        //nrf_saadc_channel_init(NRF_SAADC, config->adc_channels[i], &cfg);
-        //nrf_saadc_channel_input_set(NRF_SAADC, config->adc_channels[i], NRF_SAADC_INPUT_DISABLED, NRF_SAADC_INPUT_DISABLED);
+        nrf_saadc_channel_init(NRF_SAADC, config->adc_channels[i], &cfg);
+        nrf_saadc_channel_input_set(NRF_SAADC, config->adc_channels[i], NRF_SAADC_INPUT_DISABLED, NRF_SAADC_INPUT_DISABLED);
     }
     // init pwm
 
