4b29f0533ed8064f76fbaf90005fde6775d43996
Nan_o <litmus337@gmail.com>
2025-04-29 01:14:08 +0900

    h

--- src/babopad.c
+++ src/babopad.c
@@ -44,7 +44,7 @@ static struct adc_sequence sequence = {
 static inline void filter(int* _x, int* _y, int* _total)
 {
     static float q[3] = { 0.15, 0.15, 0.25 };
-    static float r[3] = { 32, 32, 1024 };
+    static float r[3] = { 12, 12, 32 };
     static float x[3] = { 0, 0, 1000 };
     static float v[3] = { 0, 0, 0 };
     static float p[3] = { 127, 127, 4095 };
