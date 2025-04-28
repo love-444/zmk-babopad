7f210a73b28e39038c663e72f15631d42635a07c
Nan_o <litmus337@gmail.com>
2025-04-29 01:10:55 +0900

    n

--- src/babopad.c
+++ src/babopad.c
@@ -43,7 +43,7 @@ static struct adc_sequence sequence = {
 
 static inline void filter(int* _x, int* _y, int* _total)
 {
-    static float q[3] = { 0.5, 0.5, 10 };
+    static float q[3] = { 0.15, 0.15, 0.25 };
     static float r[3] = { 32, 32, 1024 };
     static float x[3] = { 0, 0, 1000 };
     static float v[3] = { 0, 0, 0 };
@@ -114,8 +114,8 @@ static int babopad_report_data(const struct device *dev) {
         y_b = y;
         return;
     }
-    input_report(dev, config->evt_type, config->input_code_x, 6 * (x - x_b), false, K_FOREVER);
-    input_report(dev, config->evt_type, config->input_code_y, -6 * (y - y_b), true, K_FOREVER);
+    input_report(dev, config->evt_type, config->input_code_x, 3 * (x - x_b), false, K_FOREVER);
+    input_report(dev, config->evt_type, config->input_code_y, -3 * (y - y_b), true, K_FOREVER);
     x_b = x;
     y_b = y;
     return 0;
